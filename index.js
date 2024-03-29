540b8284a49806b1a0842dbcf2ca0ff0e2269b39
mariamiqbal9251 <163730535+mariamiqbal9251@users.noreply.github.com>
2024-03-29 12:28:13 +0500

    'Commits'

---
+++ index.js
@@ -0,0 +1,43 @@
+import inquirer from "inquirer";
+let firstNumber = await inquirer.prompt([{
+        name: 'value1',
+        message: 'Enter first value',
+        type: 'number',
+    }]);
+let secondNumber = await inquirer.prompt([{
+        name: 'value2',
+        message: 'Enter second value',
+        type: 'number',
+    }]);
+console.log("Press + for addition");
+console.log("Press - for subtraction");
+console.log("Press * for multiplication");
+console.log("Press / for division");
+let operator = await inquirer.prompt([{
+        name: 'operator',
+        message: 'Enter Action to be performed',
+        type: 'input',
+    }]);
+const val1 = firstNumber.value1;
+const val2 = secondNumber.value2;
+const op = operator.operator;
+if (op == '+') {
+    console.log("Addition is " + (val1 + val2));
+}
+else if (op == '-') {
+    console.log("Subtraction is " + (val1 - val2));
+}
+else if (op == '*') {
+    console.log("Multiplication is " + (val1 * val2));
+}
+else if (op == '/') {
+    if (val1.value2 == 0) {
+        console.log("Error: Division by zero");
+    }
+    else {
+        console.log("Division is " + (val1 / val2));
+    }
+}
+else {
+    console.log("Invalid operator");
+}
